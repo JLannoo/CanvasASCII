ce2e5494d7166d720386dbc6d34c76248a0f511f
JLannoo <julianlannoo@gmail.com>
2022-10-26 00:02:08 -0300

    Add drag-n-drop for file uploads

--- form.js
+++ form.js
@@ -1,7 +1,11 @@
 import generateImage from './canvas.js';
+import { fileDropErrorHandling } from './utilities/error.js';
 
 const form = document.querySelector("form");
+
 const select = document.querySelector("#select");
+const linkInput = document.querySelector("#link");
+const fileInput = document.querySelector("#file");
 const colorRangeInput = document.querySelector("#colorDepth");
 const numberInput = document.querySelector("#pixelSize");
 const asciiCheckbox = document.querySelector("#ascii");
@@ -11,6 +15,8 @@ const brightnessCompensationRange = document.querySelector("#brightnessCompensat
 const brightnessCompensationValue = document.querySelector("#brightnessCompensationValue");
 
 const error = document.querySelector("#error");
+
+const fileDropOverlay = document.querySelector("dialog");
 
 let generated = false;
 
@@ -35,7 +41,46 @@ form.addEventListener("submit", async (e) => {
     sendGenerateSignal();
 
     generated = true;
-})
+});
+
+document.body.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    fileDropOverlay.open = true;
+});
+document.body.addEventListener("dragend", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    fileDropOverlay.open = false;
+});
+
+fileDropOverlay.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    fileDropOverlay.open = false;
+});
+
+fileDropOverlay.addEventListener("drop", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    fileDropOverlay.open = false;
+
+    try {
+        displayError("");
+        fileDropErrorHandling(e);
+    } catch (err) {
+        return displayError(err.message);
+    }
+    
+    select.value = "file";
+    select.dispatchEvent(new Event("change"));
+
+    fileInput.files = e.dataTransfer.files;
+});
+
 
 brightnessCompensationRange.addEventListener("input", (e) => {
     brightnessCompensationValue.textContent = e.target.value;
--- utilities/error.js
+++ utilities/error.js
@@ -16,3 +16,19 @@ export function errorHandling(data){
     if(data.colorDepth <= 0) throw new Error("Color depth must be greater than 0");
     if(data.colorDepth > 255) throw new Error("Color depth must be less than 256");
 }
+
+/**
+ * Throw error if the files dropped are not valid.
+ * @param {DragEvent} e 
+ */
+export function fileDropErrorHandling(e){
+    if(e && e.dataTransfer && e.dataTransfer.items){
+        if(e.dataTransfer.items.length !== 1) throw new Error("Only one file can be dropped at a time");
+
+        if(e.dataTransfer.items[0].kind !== "file") throw new Error("Only files can be dropped");
+
+        if(!e.dataTransfer.items[0].type.startsWith("image/")) throw new Error("Only images can be dropped");    
+    } else {
+        throw new Error("Error while reading dropped files");
+    }
+}
